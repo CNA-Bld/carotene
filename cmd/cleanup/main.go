d13813529662e3f08067fb5e6382dc067a21e40f
CNA-Bld <cna.bld@gmail.com>
2022-11-26 23:57:02 +0800

    Parallelize cleanup

--- cmd/cleanup/main.go
+++ cmd/cleanup/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"github.com/CNA-Bld/carotene/internal/utils"
 	"github.com/otiai10/copy"
 	"github.com/vmihailenco/msgpack/v5"
+	"golang.org/x/sync/errgroup"
 	"os"
 	"path"
 	"path/filepath"
@@ -24,64 +26,73 @@ func main() {
 
 	baseMoveToDir := filepath.Join(p, "Backup")
 
+	g, _ := errgroup.WithContext(context.Background())
+	g.SetLimit(16)
+
 	for _, f := range files {
-		fileName := f.Name()
-		if f.IsDir() || !strings.HasSuffix(fileName, ".msgpack") {
-			return
-		}
-
-		mtSegs := []string{baseMoveToDir}
-		if t, err := utils.TimeFromFileName(fileName); err == nil {
-			mtSegs = append(mtSegs, t.Format("20060102"))
-		}
-		moveToPath := path.Join(mtSegs...)
-
-		filePath := filepath.Join(p, fileName)
-		if strings.HasSuffix(fileName, "R.msgpack") {
-			info, err := f.Info()
-			if err != nil {
-				panic(err)
+		f := f
+		g.Go(func() error {
+			fileName := f.Name()
+			if f.IsDir() || !strings.HasSuffix(fileName, ".msgpack") {
+				return nil
 			}
-			if info.Size() < 512*1024 {
-				copyToSeg := ""
 
-				b, err := os.ReadFile(filePath)
+			mtSegs := []string{baseMoveToDir}
+			if t, err := utils.TimeFromFileName(fileName); err == nil {
+				mtSegs = append(mtSegs, t.Format("20060102"))
+			}
+			moveToPath := path.Join(mtSegs...)
+
+			filePath := filepath.Join(p, fileName)
+			if strings.HasSuffix(fileName, "R.msgpack") {
+				info, err := f.Info()
 				if err != nil {
 					panic(err)
 				}
-				d := make(map[string]interface{})
-				if err := msgpack.Unmarshal(b, &d); err == nil {
-					// Just continue if we fail to unmarshal. Some response packets adhere to msgpack specs but not JSON.
-					if d, ok := d["data"]; ok {
-						data := d.(map[string]interface{})
+				if info.Size() < 512*1024 {
+					copyToSeg := ""
 
-						if has(data, "race_start_params_array") && has(data, "race_result_array") && has(data, "rp_info") {
-							// Team Races
-							copyToSeg = "TeamRaces"
-						} else if has(data, "room_info") && has(data, "race_horse_data_array") {
-							// Champions Meeting
-							copyToSeg = "RoomRaces"
-						} else if has(data, "circle_info") && has(data, "circle_user_array") {
-							// Circle Detail
-							copyToSeg = "CircleDetails"
-						}
-					}
-				}
-
-				if copyToSeg != "" {
-					err := copy.Copy(filePath, filepath.Join(p, copyToSeg, fileName), copy.Options{PreserveTimes: true})
+					b, err := os.ReadFile(filePath)
 					if err != nil {
 						panic(err)
 					}
+					d := make(map[string]interface{})
+					if err := msgpack.Unmarshal(b, &d); err == nil {
+						// Just continue if we fail to unmarshal. Some response packets adhere to msgpack specs but not JSON.
+						if d, ok := d["data"]; ok {
+							data := d.(map[string]interface{})
+
+							if has(data, "race_start_params_array") && has(data, "race_result_array") && has(data, "rp_info") {
+								// Team Races
+								copyToSeg = "TeamRaces"
+							} else if has(data, "room_info") && has(data, "race_horse_data_array") {
+								// Champions Meeting
+								copyToSeg = "RoomRaces"
+							} else if has(data, "circle_info") && has(data, "circle_user_array") {
+								// Circle Detail
+								copyToSeg = "CircleDetails"
+							}
+						}
+					}
+
+					if copyToSeg != "" {
+						err := copy.Copy(filePath, filepath.Join(p, copyToSeg, fileName), copy.Options{PreserveTimes: true})
+						if err != nil {
+							panic(err)
+						}
+					}
 				}
 			}
-		}
 
-		_ = os.MkdirAll(moveToPath, os.ModeDir)
-		err := os.Rename(filePath, filepath.Join(moveToPath, fileName))
-		if err != nil {
-			panic(err)
-		}
+			_ = os.MkdirAll(moveToPath, os.ModeDir)
+			err := os.Rename(filePath, filepath.Join(moveToPath, fileName))
+			if err != nil {
+				panic(err)
+			}
+			return nil
+		})
 	}
-
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/otiai10/copy v1.6.0
 	github.com/vmihailenco/msgpack/v5 v5.3.4
+	golang.org/x/sync v0.1.0
 )
 
 require github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
